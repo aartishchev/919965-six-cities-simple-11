c9941c9038cb6a2ec668e090b66e7e83b08ab4e7
Артищев Алексей <artishev@satel.org>
2022-12-04 23:48:54 +0300

    fix empty list component

--- project/src/components/empty-list/empty-list.tsx
+++ project/src/components/empty-list/empty-list.tsx
@@ -1,10 +1,18 @@
-function EmptyList(): JSX.Element {
+import { Cities } from '../../const';
+
+type EmptyListProps = {
+  selectedCity: typeof Cities[number];
+};
+
+function EmptyList({ selectedCity }: EmptyListProps): JSX.Element {
   return (
     <div className="cities__places-container cities__places-container--empty container">
       <section className="cities__no-places">
         <div className="cities__status-wrapper tabs__content">
           <b className="cities__status">No places to stay available</b>
-          <p className="cities__status-description">We could not find any property available at the moment in Dusseldorf</p>
+          <p className="cities__status-description">
+            We could not find any property available at the moment in {selectedCity}
+          </p>
         </div>
       </section>
       <div className="cities__right-section"></div>
